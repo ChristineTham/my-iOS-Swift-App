bc1e88b1e071912a23b1e5420fbd20168284979c
Christine Tham <chris.tham@hellotham.com>
2018-11-30 17:18:23 +1100

    Initial Commit

---
+++ my iOS Swift App/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  my iOS Swift App
+//
+//  Created by Chris Tham on 30/11/18.
+//  Copyright © 2018 Hello Tham. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ my iOS Swift App/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  my iOS Swift App
+//
+//  Created by Chris Tham on 30/11/18.
+//  Copyright © 2018 Hello Tham. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
