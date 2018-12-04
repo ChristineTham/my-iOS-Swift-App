b752352443b004c3dee89af7e37bcf446e71835e
Christine Tham <chris.tham@hellotham.com>
2018-12-04 16:37:50 +1100

    Cleaned out snippet code, added functions to MSGraphController (untested)

--- my iOS Swift App/MSGraphController.swift
+++ my iOS Swift App/MSGraphController.swift
@@ -17,13 +17,18 @@ class MSGraphController {
         return client!
     }()
     var me : MSGraphUser?
+    var myImage : UIImage?
     var allUsers : [MSGraphUser]
-    var myGroups : [MSGraphGroup]
+    var myGroups : [MSGraphDirectoryObject]
+    var myEvents : [MSGraphEvent]
 
     init (with authentication: Authentication) {
         MSGraphClient.setAuthenticationProvider(authentication.authenticationProvider)
+        me = nil
+        myImage = nil
         allUsers = []
         myGroups = []
+        myEvents = []
     }
     
     // Returns select information about the signed-in user from Azure Active Directory. Applies to personal or work accounts
@@ -76,49 +81,235 @@ class MSGraphController {
             }
         }
     }
-}
-
-// MARK: - Enum Result
-enum Result {
-    case Success(displayText: String?)
-    case SuccessDownloadImage(displayImage: UIImage?)
-    case Failure(error: Error)
-}
-
-// MARK: - Protocol Snippet
-protocol Snippet {
-    func execute(with completion:(_ result: Result) -> Void)
     
-    var needAdminAccess: Bool { get }
-    var name: String { get }
-}
-
-extension Snippet {
-    func execute(with completion:(_ result: Result) -> Void) {
-        assert(true, "Empty execution body")
+    // Get user's direct reports
+    // Applies to work accounts only
+    func getDirects(with completion: @escaping (_ result: Result) -> Void) {
+        graphClient.me().directReports().request().getWithCompletion {
+            (directCollection: MSCollection?, nextRequest: MSGraphUserDirectReportsCollectionWithReferencesRequest?, error: Error?) in
+            
+            if let nsError = error {
+                completion(.Failure(error: MSGraphError.NSErrorType(error: nsError as NSError)))
+            }
+            else {
+                var displayString = "List of directs: \n"
+                if let directs = directCollection {
+                    
+                    for direct: MSGraphDirectoryObject in directs.value as! [MSGraphDirectoryObject] {
+                        guard let name = direct.dictionaryFromItem()["displayName"] else {
+                            completion(.Failure(error: MSGraphError.UnexpectecError(errorString: "Display name not found")))
+                            return
+                        }
+                        displayString += "\(name)\n"
+                    }
+                }
+                
+                if let _ = nextRequest {
+                    displayString += "Next request available for more users"
+                }
+                
+                completion(.Success(displayText: "\(displayString)"))
+            }
+        }
     }
-}
-
-struct Snippets {
     
-    static var graphClient: MSGraphClient = {
-        return MSGraphClient.defaultClient()
-    }()
-}
-
-enum MSGraphError: Error {
-    case NSErrorType(error: NSError)
-    case UnexpectecError(errorString: String)
-}
-
-// Gets a collection of groups that the signed-in user is a member of.
-struct GetUserGroups: Snippet {
-    let name = "Get user groups"
-    let needAdminAccess: Bool = true
-    
-    func execute(with completion: @escaping (_ result: Result) -> Void) {
+    // Get user's manager if they have one.
+    // Applies to work accounts only
+    func getManager(with completion: @escaping (_ result: Result) -> Void) {
+        graphClient.me().manager().request().getWithCompletion {
+            (directoryObject: MSGraphDirectoryObject?, error: Error?) in
+            
+            if let nsError = error {
+                completion(.Failure(error: MSGraphError.NSErrorType(error: nsError as NSError)))
+            }
+            else {
+                var displayString: String = "Manager information: \n"
+                
+                if let manager = directoryObject {
+                    if let managerName = manager.dictionaryFromItem()["displayName"] {
+                        displayString += "Manager is \(managerName)\n\n"
+                    }
+                    else {
+                        displayString += "No manager"
+                    }
+                    displayString += "Full object is\n\(manager)"
+                }
+                completion(.Success(displayText: "\(displayString)"))
+            }
+        }
+    }
         
-        Snippets.graphClient.me().memberOf().request().getWithCompletion {
+    // Gets the signed-in user's photo data if they have a photo.
+    // Applies to work accounts only
+    func getPhoto(with completion: @escaping (_ result: Result) -> Void) {
+            
+        graphClient.me().photoValue().download {
+            (url: URL?, response: URLResponse?, error: Error?) in
+                
+            if let nsError = error {
+                completion(.Failure(error: MSGraphError.NSErrorType(error: nsError as NSError)))
+                return
+            }
+            
+            guard let picUrl = url else {
+                completion(.Failure(error: MSGraphError.UnexpectecError(errorString: "No downloaded URL")))
+                return
+            }
+            
+            let picData = try? Data(contentsOf: picUrl)
+            self.myImage = UIImage(data: picData!)
+            
+            completion(.SuccessDownloadImage(displayImage: self.myImage))
+        }
+    }
+
+    // Gets the signed-in user's events.
+    // Applies to personal or work accounts
+    func getEvents(with completion: @escaping (_ result: Result) -> Void) {
+        graphClient.me().events().request().getWithCompletion {
+            (eventCollection: MSCollection?, nextRequest: MSGraphUserEventsCollectionRequest?, error: Error?) in
+            if let nsError = error {
+                completion(.Failure(error: MSGraphError.NSErrorType(error: nsError as NSError)))
+            }
+            else {
+                var displayString = "List of events (subjects):\n"
+                if let events = eventCollection {
+                    self.myEvents = []
+                    
+                    for event: MSGraphEvent in events.value as! [MSGraphEvent] {
+                        displayString += event.subject + "\n\n"
+                        self.myEvents.append(event)
+                    }
+                }
+                
+                if let _ = nextRequest {
+                    displayString += "Next request available for more users"
+                }
+                
+                completion(.Success(displayText: displayString))
+            }
+        }
+    }
+    
+    // Create an event in the signed in user's calendar.
+    // Applies to personal or work accounts
+    func createEvent(with completion: @escaping (_ result: Result) -> Void) {
+        
+        let event = createEventObject(isSeries: false)
+        
+        graphClient.me().calendar().events().request().add(event) {
+            (event: MSGraphEvent?, error: Error?) in
+            if let nsError = error {
+                completion(.Failure(error: MSGraphError.NSErrorType(error: nsError as NSError)))
+            }
+            else {
+                let displayString = "Event created with id \(event!.entityId!)"
+                completion(.Success(displayText: displayString))
+            }
+        }
+    }
+    
+    // Updates an event in the signed in user's calendar.
+    // Applies to personal or work accounts
+    func updateEvent(with completion: @escaping (_ result: Result) -> Void) {
+        
+        // Enter a valid event id
+        let eventId = "ENTER_VALID_EVENT_ID"
+        
+        // Get an event and then update
+        graphClient.me().events(eventId).request().getWithCompletion {
+            (event: MSGraphEvent?, error: Error?) in
+            
+            if let nsError = error {
+                completion(.Failure(error: MSGraphError.NSErrorType(error: nsError as NSError)))
+            }
+            else {
+                guard let validEvent = event else {
+                    completion(.Failure(error: MSGraphError.UnexpectecError(errorString: "Event ID not returned")))
+                    return
+                }
+                validEvent.subject = "New Name"
+                self.graphClient.me().events(validEvent.entityId).request().update(validEvent, withCompletion: {
+                    (updatedEvent: MSGraphEvent?, error: Error?) in
+                    if let nsError = error {
+                        completion(.Failure(error: MSGraphError.NSErrorType(error: nsError as NSError)))
+                    }
+                    else {
+                        let displayString = "Event updated with a new subject"
+                        completion(.Success(displayText: displayString))
+                    }
+                })
+            }
+        }
+    }
+    
+    // Deletes an event in the signed in user's calendar.
+    // Applies to personal or work accounts
+    func deleteEvent(with completion: @escaping (_ result: Result) -> Void) {
+        
+        // Enter a valid event id
+        let eventId = "ENTER_VALID_EVENT_ID"
+        
+        graphClient.me().events(eventId).request().delete(completion: {
+            (error: Error?) in
+            if let nsError = error {
+                completion(.Failure(error: MSGraphError.NSErrorType(error: nsError as NSError)))
+            }
+            else {
+                completion(.Success(displayText: "Deleted calendar event id: \(eventId)"))
+            }
+        })
+    }
+    // Helper for creating a event object.
+    // Set series to true to create a recurring event.
+    func createEventObject(isSeries series: Bool) -> MSGraphEvent {
+        
+        let event = MSGraphEvent()
+        event.subject = "Event Subject"
+        event.body = MSGraphItemBody()
+        event.body.contentType = MSGraphBodyType.text()
+        event.body.content = "Sample event body"
+        event.importance = MSGraphImportance.normal()
+        
+        let startDate: Date = Date(timeInterval: 30 * 60, since: Date())
+        let endDate: Date = Date(timeInterval: 30 * 60, since: startDate)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss"
+        
+        event.start = MSGraphDateTimeTimeZone()
+        event.start.dateTime = dateFormatter.string(from: startDate)
+        
+        // For more timezone settings, visit this link
+        // http://graph.microsoft.io/en-us/docs/api-reference/v1.0/resources/datetimetimezone
+        event.start.timeZone = "Pacific/Honolulu"
+        
+        event.end = MSGraphDateTimeTimeZone()
+        event.end.dateTime = dateFormatter.string(from: endDate)
+        event.end.timeZone = "Pacific/Honolulu"
+        
+        if !series {
+            event.type = MSGraphEventType.singleInstance()
+        }
+        else {
+            event.type = MSGraphEventType.seriesMaster()
+            
+            event.recurrence = MSGraphPatternedRecurrence()
+            event.recurrence.pattern = MSGraphRecurrencePattern()
+            event.recurrence.pattern.interval = 1
+            event.recurrence.pattern.type = MSGraphRecurrencePatternType.weekly()
+            event.recurrence.pattern.daysOfWeek = [MSGraphDayOfWeek.friday()]
+            event.recurrence.range = MSGraphRecurrenceRange()
+            event.recurrence.range.type = MSGraphRecurrenceRangeType.noEnd()
+            event.recurrence.range.startDate = MSDate(nsDate: startDate)
+        }
+        return event
+    }
+
+    // Gets a collection of groups that the signed-in user is a member of.
+    func getGroups(with completion: @escaping (_ result: Result) -> Void) {
+        
+        graphClient.me().memberOf().request().getWithCompletion {
             (userGroupCollection: MSCollection?,
             nextRequest: MSGraphUserMemberOfCollectionWithReferencesRequest?,
             error: Error?) in
@@ -131,12 +322,14 @@ struct GetUserGroups: Snippet {
             var displayString = "List of groups: \n"
             
             if let userGroups = userGroupCollection {
+                self.myGroups = []
                 for userGroup: MSGraphDirectoryObject in userGroups.value as! [MSGraphDirectoryObject] {
                     guard let name = userGroup.dictionaryFromItem()["displayName"] else {
                         completion(.Failure(error: MSGraphError.UnexpectecError(errorString: "Display name not found")))
                         return
                     }
                     displayString += "\(name)\n"
+                    self.myGroups.append(userGroup)
                 }
             }
             if let _ = nextRequest {
@@ -146,4 +339,16 @@ struct GetUserGroups: Snippet {
             completion(.Success(displayText: displayString))
         }
     }
+}
+
+// MARK: - Enum Result
+enum Result {
+    case Success(displayText: String?)
+    case SuccessDownloadImage(displayImage: UIImage?)
+    case Failure(error: Error)
+}
+
+enum MSGraphError: Error {
+    case NSErrorType(error: NSError)
+    case UnexpectecError(errorString: String)
 }
